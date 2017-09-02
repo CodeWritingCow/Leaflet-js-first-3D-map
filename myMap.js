5e300d246c8b0be9b00136bc665d0c65a2958dcd
Gary Pang <gary.pang@gmail.com>
2017-09-02 05:04:21 -0400

    Add second map with user's location as center
    Get user's coordinates with geolocation API.

--- myMap.js
+++ myMap.js
@@ -10,3 +10,27 @@ var marker = L.marker([40.713, -74.006]).addTo(myMap);
 	    L.Wrld.themes.time.Day,
 		L.Wrld.themes.weather.Clear
 		);
+
+
+// var userMap = L.Wrld.map("userMap", config.MY_KEY);
+var userMap = L.map("userMap");
+
+
+// Find user's location via the geolocation API
+function getLocation() {
+	navigator.geolocation.getCurrentPosition(function(position) {
+		var userSpot = [position.coords.latitude, position.coords.longitude];
+		var marker1 = L.marker(userSpot).addTo(userMap);
+		marker1.bindPopup("This is your current location: " + userSpot);
+
+		userMap.setView(userSpot, 16); // Set map center
+	});
+}
+
+// Add National Geographic tile layer to map
+L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
+	maxZoom: 16
+}).addTo(userMap);
+
+getLocation();
