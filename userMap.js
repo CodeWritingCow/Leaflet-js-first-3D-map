f580b777c7442f6cc7ad7e9b195aaf9e2cd4cccd
Gary Pang <gary.pang@gmail.com>
2017-09-03 13:08:48 -0400

    Add links to icons
    Add links to social media icons. Also, refactor userMaps.js, remove
    extraneous code.

--- userMap.js
+++ userMap.js
@@ -2,8 +2,9 @@ var worldCenter = [30, 31];
 var userSpot = [0 ,0];
 
 var userMap = L.map("userMap");
-	// userMap.setView(worldCenter, 1);
-	
+	userMap.setView(worldCenter, 1);
+
+var userMarker;
 
 // Add NASA night tile layer to map
 L.tileLayer('http://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_CityLights_2012/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}', {
@@ -17,15 +18,26 @@ L.tileLayer('http://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_CityLights_20
 }).addTo(userMap);
 
 
-// Find user's location using Leaflet's locate method with the setView option
-function onLocationFound(e) {
-    var radius = e.accuracy / 2;
-
-    L.marker(e.latlng).addTo(userMap)
-        .bindPopup("You are within " + radius + " meters from this point").openPopup();
-
-    L.circle(e.latlng, radius).addTo(userMap);
+// Find user's geolocation using Leaflet's locate method
+function findLocation() {
+	userMap.locate({setView: true, maxZoom: 3});
 }
+
+
+// If Leaflet finds user's geolocation, set marker there
+function onLocationFound(e) {
+    // var radius = e.accuracy / 2;
+    userMarker = L.marker(e.latlng);
+    	userMarker.addTo(userMap);
+        userMarker.bindPopup("You are here").openPopup();
+}
+
+// Reset map zoom to 1, and reset center to worldCenter coordinates. Remove marker.
+function resetLocation() {
+	userMap.removeLayer(userMarker);
+	userMap.setView(worldCenter, 1);
+}
+
 
 // If Leaflet fails to find user's geolocation, show error message.
 // Set map center to worldCenter coordinates.
@@ -36,5 +48,3 @@ function onLocationError(e) {
 
 userMap.on('locationfound', onLocationFound);
 userMap.on('locationerror', onLocationError);
-
-userMap.locate({setView: true, maxZoom: 1});
