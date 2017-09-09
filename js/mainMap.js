5e5dc487cacf4e93185efecaf633933020131399
Gary Pang <gary.pang@gmail.com>
2017-09-09 15:56:16 -0400

    Enable indoor maps
    Users can access indoor maps for some buildings on 3D map.

--- js/mainMap.js
+++ js/mainMap.js
@@ -1,7 +1,8 @@
 var mainMap = L.Wrld.map("map", config.MY_KEY, {
 	center: [40.709138, -73.995889],
 	zoom: 14.5,
-	zoomControl: true // wrld.js appears to disable zoomControl by default
+	zoomControl: true, // wrld.js appears to disable zoomControl by default
+	indoorsEnabled: true // Allows users to access indoor maps. Entrance markers appear on world map.
 });
 
 /* var marker = L.marker([40.713, -74.006]);
@@ -15,3 +16,26 @@ mainMap.themes.setTheme(
     L.Wrld.themes.time.Day,
 	L.Wrld.themes.weather.Clear
 	);
+
+
+function onIndoorMapEntered(event) {        
+var latLng = mainMap.getCenter();        
+var indoorMapId = event.indoorMap.getIndoorMapId();
+var buildingName = event.indoorMap.getIndoorMapName();
+
+var popupOptions = { 
+  indoorMapId: indoorMapId, 
+  indoorMapFloorIndex: 0, 
+  autoClose: false, 
+  closeOnClick: false 
+};
+
+var popup = L.popup(popupOptions)
+  .setLatLng(latLng)
+  .addTo(mainMap)
+  .setContent(buildingName);
+
+mainMap.on("popupclose", function() { mainMap.indoors.exit(); });
+}
+
+mainMap.indoors.on("indoormapenter", onIndoorMapEntered);
